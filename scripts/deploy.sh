52f3e4e734bd4a85e80e02001154f8f09c58c775
liuyang34 <liuyang34@360.cn>
2024-01-12 16:41:14 +0800

    feat: 更新部署脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
+if [ -f yarn.lock ]
+then 
+    yarn --frozen-lockfile
+elif [ -f package-lock.json ]
+then
+    npm ci
+elif [ -f pnpm-lock.yaml ]
+then
+    yarn global add pnpm && pnpm i --frozen-lockfile
+else
+    echo "Lockfile not found." && exit 1
+fi
 
-cd /apps/wipi
-git checkout main
-git pull
-
-pnpm install
 pnpm run build
 pnpm run pm2
 
