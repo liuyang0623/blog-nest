068a7e19d3100b8386b98be30cc0d7397e8d4590
liuyang0623 <1126323596@qq.com>
2024-11-12 15:19:39 +0800

    feat: Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-if [ -f yarn.lock ]
-then 
-    yarn --frozen-lockfile
-elif [ -f package-lock.json ]
-then
-    npm ci
-elif [ -f pnpm-lock.yaml ]
-then
-    yarn config set registry https://registry.npm.taobao.org/
-    yarn global add pnpm && pnpm i --frozen-lockfile
-else
-    echo "Lockfile not found." && exit 1
-fi
+# if [ -f yarn.lock ]
+# then 
+#     yarn --frozen-lockfile
+# elif [ -f package-lock.json ]
+# then
+#     npm ci
+# elif [ -f pnpm-lock.yaml ]
+# then
+#     yarn config set registry https://registry.npm.taobao.org/
+#     yarn global add pnpm && pnpm i --frozen-lockfile
+# else
+#     echo "Lockfile not found." && exit 1
+# fi
 
 pnpm run build
 pnpm run pm2
